f77c9ecaa86dce026f7acbaee3f07527da1c2980
Lee Bock <lbock@esri.com>
2013-09-24 12:10:48 -0400

    1) conditional proxy url 2) removed some console.logs 3) queryRecsByCity needs to call callback (even though it's still a stub)

--- app/HerokuService.js
+++ app/HerokuService.js
@@ -1,11 +1,11 @@
 function HerokuService()
 {
 	
-	var SERVICE_URL = "http://localhost/proxy/proxy.ashx/?http://esri-destination.herokuapp.com/";
-	
-	//var PROXY_URL = window.location.href.toLowerCase().indexOf("storymaps.esri.com") >= 0 ? "http://storymaps.esri.com/proxy/proxy.ashx" : "http://localhost/proxy/proxy.ashx";
-	//esri.config.defaults.io.proxyUrl = PROXY_URL;
-
+	var PROXY_URL = window.location.href.toLowerCase().indexOf("storymaps.esri.com") >= 0 ? 
+						"http://storymaps.esri.com/proxy/proxy.ashx" : 
+						"http://localhost/proxy/proxy.ashx";
+						
+	var SERVICE_URL = PROXY_URL+"?http://esri-destination.herokuapp.com/"
 	
 	this.getLocations = function(callBack)
 	{
@@ -17,11 +17,8 @@ function HerokuService()
 				var arr = [];
 				var sym, pt, atts;
 				$.each(result, function(index, value) {
-					console.log(value);
 					sym = createSymbol(value.count*SYMBOL_BASE_SIZE,0.25);
-					console.log(sym);
 					pt = new esri.geometry.Point(parseFloat(value.x), parseFloat(value.y));
-					console.log(pt);
 					atts = new LocationRec(value.standardized_name, value.standardized_name, value.count);
 					arr.push(new esri.Graphic(pt, sym, atts));
 				});
@@ -33,7 +30,7 @@ function HerokuService()
 	
 	this.queryRecsByCity = function(name, callBack)
 	{
-		return [];
+		callBack([]);
 	}
 	
 }
