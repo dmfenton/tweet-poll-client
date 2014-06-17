4bc35e78c4b39fc3fdf9efb1b6f37d76bf91c785
Lee Bock <lbock@esri.com>
2014-06-17 10:31:14 -0400

    point config to GenericMapTest service

--- app/Config.js
+++ app/Config.js
@@ -2,10 +2,10 @@ function Config() {
 }
 
 Config.BASEMAP_SERVICE = "http://tiles.arcgis.com/tiles/nGt4QxSblgDfeJn9/arcgis/rest/services/DGCM_2Msmaller_BASE/MapServer";
-Config.FEATURE_SERVICE_URL = "http://services.arcgis.com/nzS0F0zdNLvs7nc8/arcgis/rest/services/ISS_Tweets/FeatureServer/0";
+Config.FEATURE_SERVICE_URL = "http://services.arcgis.com/nzS0F0zdNLvs7nc8/arcgis/rest/services/GenericTestTweets/FeatureServer/0";
 Config.PARAMETER_STANDARDIZEDNAME = "standardizedName";
 
-Config.TWEET_KEYWORD = "#TestISSMap"
+Config.TWEET_KEYWORD = "#GenericMapTest"
 Config.TWEET_INTENT_TEXT = "Lorem Ipsum {New York, New York}";
 //Config.BITLY = "";
 
